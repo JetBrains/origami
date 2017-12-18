1faa3bd551ba54b7ee913d56917cbb09d8917168
skotin <sergo.golovachev@gmail.com>
2017-12-18 15:02:17 +0100

    view triangle

--- Main.elm
+++ Main.elm
@@ -1,10 +1,18 @@
 module Main exposing (main)
 
 import AnimationFrame
-import Html exposing (Html, text)
+import Html exposing (Html, text, div)
+import Html.Attributes exposing (width, height, style)
 import Task exposing (Task)
 import Time exposing (Time)
+import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector3 as Vec3 exposing (Vec3, vec3)
+import WebGL exposing (Mesh, Shader, Entity)
 import Window
+
+
+numVertices : Int
+numVertices = 500
 
 
 type alias Model =
@@ -16,12 +24,17 @@ type alias Model =
     , paused : Bool
     }
 
+type alias Vertex =
+    { position : Vec3
+    , color : Vec3
+    }
 
 type Msg
     = Animate Time
     | Resize Window.Size
     | Pause
     | Start
+
 
 init : ( Model, Cmd Msg )
 init =
@@ -38,12 +51,40 @@ init =
         ]
     )
 
+
+lorenz : Model -> Time -> ( Vertex, Vertex, Vertex )
+lorenz _ _ =
+    ( Vertex (vec3 0 0 0) (vec3 1 0 0)
+    , Vertex (vec3 1 1 0) (vec3 0 1 0)
+    , Vertex (vec3 1 -1 0) (vec3 0 0 1)
+    )
+
 view : Model -> Html Msg
 view model =
     let
         a = "foo"
     in
-        text (toString model.sigma ++ a)
+        div [ ]
+            [ text (toString model.sigma ++ a)
+            , WebGL.toHtml
+                [ width 400
+                , height 400
+                , style [ ( "display", "block" ) ]
+                ]
+                [ WebGL.entity
+                    vertexShader
+                    fragmentShader
+                    (WebGL.triangles [ lorenz model 10000 ])
+                    { perspective = perspective 100 }
+                ]
+            ]
+
+
+perspective : Float -> Mat4
+perspective t =
+    Mat4.mul
+        (Mat4.makePerspective 45 1 0.01 100)
+        (Mat4.makeLookAt (vec3 (4 * cos t) 0 (4 * sin t)) (vec3 0 0 0) (vec3 0 1 0))
 
 
 subscriptions : Model -> Sub Msg
@@ -71,3 +112,34 @@ main =
         , subscriptions = subscriptions
         , update = update
         }
+
+-- Shaders
+
+
+type alias Uniforms =
+    { perspective : Mat4 }
+
+
+vertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
+vertexShader =
+    [glsl|
+        attribute vec3 position;
+        attribute vec3 color;
+        uniform mat4 perspective;
+        varying vec3 vcolor;
+        void main () {
+            gl_Position = perspective * vec4(position, 1.0);
+            vcolor = color;
+        }
+    |]
+
+
+fragmentShader : Shader {} Uniforms { vcolor : Vec3 }
+fragmentShader =
+    [glsl|
+        precision mediump float;
+        varying vec3 vcolor;
+        void main () {
+            gl_FragColor = vec4(vcolor, 1.0);
+        }
+    |]
