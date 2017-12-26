e45b3f2f0243caf7b23f934ff9b8bf82cfb4875f
Ulric Wilfred <shaman.sir@gmail.com>
2017-12-27 00:47:08 +0100

    add sample patch to the page

--- patch.js
+++ patch.js
@@ -20,4 +20,36 @@ console.log(Rpd);
 Rpd.renderNext('svg', document.getElementById('patch-target'),
                 { style: 'ableton' });
 
-Rpd.addPatch('Lorenz').resizeCanvas(800, 800);
+var patch = Rpd.addPatch('Lorenz').resizeCanvas(800, 800);
+
+// add Metro Node, it may generate `bang` signal with the requested time interval
+var metroNode = patch.addNode('util/metro', 'Metro').move(40, 10);
+
+// add Random Generator Node that will generate random numbers on every `bang` signal
+var randomGenNode = patch.addNode('util/random', 'Random').move(130, 20);
+randomGenNode.inlets['max'].receive(26); // set maximum value of the generated numbers
+
+// add Log Node, which will log last results of the Random Generator Node
+var logRandomNode = patch.addNode('util/log', 'Log').move(210, 60);
+randomGenNode.outlets['random'].connect(logRandomNode.inlets['what']);
+
+// define the type of the node which multiplies the incoming value by two
+var multiplyTwoNode = patch.addNode('core/basic', '* 2', {
+    process: function(inlets) {
+        return {
+            'result': (inlets.multiplier || 0) * 2
+        }
+    }
+}).move(240, 10);
+var multiplierInlet = multiplyTwoNode.addInlet('util/number', 'multiplier');
+var resultOutlet = multiplyTwoNode.addOutlet('util/number', 'result');
+
+// connect Random Generator output to the multiplying node
+var logMultiplyNode = patch.addNode('util/log', 'Log').move(370, 20);
+resultOutlet.connect(logMultiplyNode.inlets['what']);
+
+// connect Random Generator output to the multiplying node
+randomGenNode.outlets['random'].connect(multiplierInlet);
+
+// finally connect Metro node to Random Generator, so the sequence starts
+metroNode.outlets['bang'].connect(randomGenNode.inlets['bang']);
