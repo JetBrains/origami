1a712082de7085822f02de21a4219de6e3897349
Ulric Wilfred <shaman.sir@gmail.com>
2017-12-26 22:00:44 +0100

    add CSS-loaders, require ableton-SVG style of RPD

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // index.js
 'use strict';
 
-var Elm = require('./Main.elm');
-var mountNode = document.getElementById('elm-app');
+require('./node_modules/rpd/src/render/svg.css');
+require('./node_modules/rpd/src/render/shared.js');
+require('./node_modules/rpd/src/render/svg.js');
+
+require('./node_modules/rpd/src/style/ableton/svg.css');
+require('./node_modules/rpd/src/style/ableton/svg.js');
 
 var Rpd = require('./node_modules/rpd/src/rpd.js');
 console.log(Rpd);
+
+var Elm = require('./Main.elm');
+var mountNode = document.getElementById('elm-app');
 
 // The third value on embed are the initial values for incomming ports into Elm
 var app = Elm.Main.embed(mountNode);
