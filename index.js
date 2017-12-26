ef11a0bcaebfa4d5f0bf7321ebda9957dd87f90f
Ulric Wilfred <shaman.sir@gmail.com>
2017-12-26 10:34:36 +0100

    test using Rpd and add ports for controlling Lorenz from outside

--- index.js
+++ index.js
@@ -4,5 +4,8 @@
 var Elm = require('./Main.elm');
 var mountNode = document.getElementById('elm-app');
 
+var Rpd = require('./node_modules/rpd/src/rpd.js');
+console.log(Rpd);
+
 // The third value on embed are the initial values for incomming ports into Elm
 var app = Elm.Main.embed(mountNode);
