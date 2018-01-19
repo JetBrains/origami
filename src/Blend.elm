f9fa0f36f8c9f846d8fb435fe7bd58bf5ee05661
Ulric Wilfred <shaman.sir@gmail.com>
2018-01-19 18:00:16 +0100

    a commit before giving up on Photoshop-like blending modes

--- src/Blend.elm
+++ src/Blend.elm
@@ -7,6 +7,19 @@ module Blend exposing
 import WebGL.Settings exposing (Setting)
 import WebGL.Settings.Blend as B
 
+{- To support complex blending methods like the ones from Photoshop,
+   it is required either to have WebGL2 enabled and use MIN/MAX extentions, or
+   use special extension in WebGL1, since their formulae are based on testing if source/destination color is minimum/maximum, and not only this:
+
+   https://developer.mozilla.org/en-US/docs/Web/API/WebGLRenderingContext/blendEquationSeparate
+   https://www.andersriggelsen.dk/glblendfunc.php
+
+
+   It is possible to implement them using shaders though, but in this case we have to have the
+   source texture: http://www.nutty.ca/articles/blend_modes/ (here all the conversion formulae
+   are listed).
+-}
+
 
 type Blend
     = Default
@@ -39,4 +52,9 @@ type Blend
 
 produce : Blend -> Setting
 produce blend =
-    B.add B.one B.zero
+    case blend of
+        Default -> B.add B.one B.zero
+        Add -> B.add B.one B.one
+        Subtract -> B.subtract B.one B.one
+        Multiply -> B.add B.dstColor B.zero
+        _ -> B.add B.one B.zero
