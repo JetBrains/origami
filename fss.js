d7a9a229e770695080084cc1c056da4e8255789e
Ulric Wilfred <shaman.sir@gmail.com>
2018-02-23 17:06:37 +0100

    learn to load FSS code in app

---
+++ fss.js
@@ -0,0 +1,7 @@
+require('./node_modules/flat-surface-shader/source/Core');
+require('./node_modules/flat-surface-shader/source/Renderer');
+const WebGLRenderer = require('./node_modules/flat-surface-shader/source/WebGLRenderer');
+
+console.log(WebGLRenderer);
+
+module.exports = WebGLRenderer;
