78c2c44b979f900bfe2e19e9928727a393a50241
Ulric Wilfred <shaman.sir@gmail.com>
2018-07-16 23:05:09 +0200

    export only the randomly generated data for the FSS scene

--- fss.js
+++ fss.js
@@ -15,7 +15,7 @@ require('./node_modules/flat-surface-shader/source/Mesh');
 require('./node_modules/flat-surface-shader/source/Scene');
 require('./node_modules/flat-surface-shader/source/Math');
 
-function buildFSS(config) {
+function buildFSS(config, source) {
     var scene = new FSS.Scene();
 
 
@@ -37,13 +37,19 @@ function buildFSS(config) {
         for (v = geometry.vertices.length - 1; v >= 0; v--) {
             vertex = geometry.vertices[v];
             vertex.anchor = FSS.Vector3.clone(vertex.position);
-            vertex.v0 = FSS.Vector3.create(
-                Math.randomInRange(0.2, 1.0),
-                Math.randomInRange(0.2, 1.0),
-                Math.randomInRange(0.2, 1.0)
-            );
-            vertex.time = Math.randomInRange(0, Math.PIM2);
-            vertex.gradient = Math.randomInRange(0.2, 0.7);
+            vertex.v0 = source && source[v]
+                ? source[v].v0
+                : FSS.Vector3.create(
+                    Math.randomInRange(0.2, 1.0),
+                    Math.randomInRange(0.2, 1.0),
+                    Math.randomInRange(0.2, 1.0)
+                  );
+            vertex.time = source && source[v]
+                ? source[v].time
+                : Math.randomInRange(0, Math.PIM2);
+            vertex.gradient = source && source[v]
+                ? source[v].gradient
+                : Math.randomInRange(0.2, 0.7);
         }
 
         light.setPosition(150, -50, 100);
