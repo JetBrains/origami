6c0077e49e8d07d3f1920988f3816b71f0f9b4cf
Ulric Wilfred <shaman.sir@gmail.com>
2018-09-04 09:45:03 +0200

    make import/export panel to be an overlay + position the text almost in the center

--- src/Layer/JbText.elm
+++ src/Layer/JbText.elm
@@ -9,14 +9,29 @@ import Svg.Attributes as SAttrs
 import InlineSvg exposing (inline)
 
 
+svgWidth = 500
+shiftX = 0
+shiftY = 0
+
+
 { icon } =
     inline
         { jetbrains = "./jetbrains-text-square.svg"
         }
 
-view : Blend.Blend -> Html a
-view blend =
-    div
-        [ HAttrs.style [("mix-blend-mode", Blend.encode blend)] ]
-        [ icon .jetbrains [ SAttrs.class "text-layer text-layer--jetbrains" ]
-        ]
+view : (Int, Int) -> Blend.Blend -> Html a
+view ( w, h ) blend =
+    let
+        posX = (w // 2) - shiftX
+        posY = (h // 2) - shiftY
+    in
+        div
+            [ HAttrs.style
+                [ ("mix-blend-mode", Blend.encode blend)
+                , ("position", "absolute")
+                , ("width", toString svgWidth ++ "px")
+                , ("top", toString posY ++ "px")
+                , ("left", toString posX ++ "px")
+                ] ]
+            [ icon .jetbrains [ SAttrs.class "text-layer text-layer--jetbrains" ]
+            ]
