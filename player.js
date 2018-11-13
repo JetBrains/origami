308b881067926e65763e6cfa1dead203330b0983
Ulric Wilfred <shaman.sir@gmail.com>
2018-11-13 16:44:43 +0100

    fix accidental save on size change

--- player.js
+++ player.js
@@ -4,21 +4,15 @@ const App = require('./src/Main.elm');
 const isFss = layer => layer.kind == 'fss' || layer.kind == 'fss-mirror';
 
 const import_ = (app, importedState) => {
-    //const parsedState = JSON.parse(importedState);
-    //debugger;
 
     const parsedState = importedState;
-    // scenes = {};
-    // layers = [];
 
     app.ports.requestFssRebuild.subscribe(({ layer : index, model, value : fssModel }) => {
         const layer = model.layers[index];
         if (isFss(layer)) {
             console.log('forced to rebuild FSS layer', index);
             const fssScene = buildFSS(model, fssModel, parsedState.layers[index].sceneFuzz);
-            // scenes[index] = fssScene;
             app.ports.rebuildFss.send({ value: fssScene, layer: index });
-            // layer.scene = fssScene;
         }
 
         app.ports.hideControls.send(null);
@@ -43,35 +37,6 @@ const import_ = (app, importedState) => {
             }
         ))
     }));
-
-    // app.ports.hideControls.send(null);
-    // app.ports.pause.send(null);
-
-    // app.ports.initLayers.send(layers.map((l) => l.kind));
-
-    // model.layers.forEach((layer, index) => {
-    //     if (isFss(layer)) {
-    //         console.log('rebuild FSS layer', index);
-    //         const fssScene = buildFSS(model, model.fss);
-    //         app.ports.rebuildFss.send({ value: fssScene, layer: index });
-    //     }
-    // });
-
-    // parsedState.layers.forEach((layer, index) => {
-    //     if (isFss(layer)) {
-    //         const scene = buildFSS(parsedState, layer.model, layer.sceneFuzz);
-    //         scenes[index] = scene;
-    //         console.log('import FSS', scene, parsedState);
-    //         //app.ports.configureFss.send({ value: layer.model, layer: index });
-    //         app.ports.rebuildFss.send({ value: scene, layer: index });
-    //     }
-    // });
-
-    // const mergedBlends = parsedState.layers.map(layer => layer.blend).join(':');
-    // window.location.hash = '#blends=' + mergedBlends;
-
-    //app.ports.continue.send(null);
-    //if (layersNode) layersNode.inlets['code'].receive(mergedBlends);
 }
 
 window.runGenScene = function() {
