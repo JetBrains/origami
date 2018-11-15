2a8ab85b4e1dd5baae7a6507b4dfe9da829b8d37
Ulric Wilfred <shaman.sir@gmail.com>
2018-11-15 15:30:02 +0100

    first steps in randomizer

--- src/Main.elm
+++ src/Main.elm
@@ -350,7 +350,7 @@ update msg model =
         ChangeLightSpeed index lightSpeed ->
             model
                 |> updateAndRebuildFssWith index
-                    (\fssModel -> { fssModel | lightSpeed = lightSpeed })
+                    (\fssModel -> { fssModel | lightSpeed = Debug.log "lightSpeed" lightSpeed })
 
         RebuildFss index serializedScene ->
             ( model |> updateLayer index
