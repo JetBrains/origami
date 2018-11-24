362cb1019921a0b5f8fba0c34da64c228953ac31
Ulric Wilfred <shaman.sir@gmail.com>
2018-11-24 19:52:00 +0100

    add some rendering to the GUI

--- src/Gui/Cell.elm
+++ src/Gui/Cell.elm
@@ -1,5 +1,6 @@
 module Gui.Cell exposing (..)
 
+import Array
 import Html exposing (Html, text, div, span, input)
 import Html.Attributes as H
 import Html.Events as H
@@ -71,35 +72,145 @@ type Msg
     -- | Color
 
 
-textAttrs xPos yPos =
-    [ fill "white"
+cellWidth = 70
+cellHeight = 70
+
+
+textAttrs : Float -> Float -> String -> List (Attribute Msg)
+textAttrs xPos yPos color =
+    [ fill color
     , x <| toString xPos, y <| toString yPos
     , fontSize "12"
     , S.style "font-family: sans-serif;"
+    , textAnchor "middle"
+    , alignmentBaseline "central"
     ]
+
+
+circleAttrs : Float -> Float -> String -> List (Attribute Msg)
+circleAttrs xPos yPos color =
+    [ cx <| toString <| xPos
+    , cy <| toString <| yPos
+    , r "20"
+    , stroke color, fill "none"
+    , strokeWidth "5"
+    ]
+
+
+upArrow : Float -> Float -> String -> Svg Msg
+upArrow xPos yPos color =
+    g
+        [ transform "rotate(-180.000000) translate(-20, -15.000000)" ]
+        [
+            Svg.polygon
+                [ transform "scale(0.25, 0.25)"
+                , x <| toString <| xPos, y <| toString <| yPos
+                --transform "translate(-37.125,-290.25)"
+                , points "83.5312 0 41.7656 61.875 0 0 16.1719 0 41.7656 37.4062 67.3594 0 83.5312 0"
+                , fill color ]
+                []
+        ]
+
+
+downArrow : Float -> Float -> String -> Svg Msg
+downArrow xPos yPos color =
+    g
+        [ transform "translate(0, 0)" ]
+        [
+            Svg.polygon
+                [ transform "scale(0.25, 0.25)"
+                , x <| toString <| xPos, y <| toString <| yPos
+                --transform "translate(-37.125,-290.25)"
+                , points "83.5312 0 41.7656 61.875 0 0 16.1719 0 41.7656 37.4062 67.3594 0 83.5312 0"
+                , fill color ]
+                []
+        ]
+
 
 
 renderCell : NestPos -> Maybe SelectionState -> Cell -> Html Msg
 renderCell position isSelected cell =
     let cellBody =
             case cell of
-                Knob label value ->
+                Knob _ value ->
                     g []
-                        [ text_ (textAttrs 35 35) [ Svg.text <| toString value ]
-                        , circle [ cx "35", cy "35", r "20", stroke "aqua", fill "none" ] []
+                        [ text_
+                            (textAttrs (cellWidth / 2) (cellHeight / 2) "aqua")
+                            [ Svg.text <| toString value ]
+                        , circle
+                            (circleAttrs (cellWidth / 2) (cellHeight / 2) "aqua")
+                            []
                         ]
-                _ -> Svg.text "?"
-        cellLabel =
+                Toggle _ state ->
+                    g []
+                        [ text_
+                            (textAttrs (cellWidth / 2) (cellHeight / 2) "aqua")
+                            [ Svg.text <| if state == TurnedOn then "on" else "off" ]
+                        , circle
+                            (circleAttrs (cellWidth / 2) (cellHeight / 2) <|
+                                if state == TurnedOn then "green" else "red")
+                            []
+                        ]
+                Choice _ state itemChosen { cells } ->
+                    g []
+                        [ text_
+                            (textAttrs (cellWidth / 2) (cellHeight / 2) "aqua")
+                            [ cells
+                                |> Array.fromList |> Array.get itemChosen
+                                |> Maybe.map cellLabel |> Maybe.withDefault "" |> Svg.text ]
+                        , g
+                            [ transform
+                                <| "translate(" ++ (toString <| cellWidth / 2 - 10) ++ ",10)" ]
+                            [ if state == Expanded
+                                then downArrow (cellWidth / 2) 10 "aqua"
+                                else upArrow (cellWidth / 2) 10 "aqua" ]
+                        ]
+                Nested _ state { cells } ->
+                    g []
+                        [ g
+                            [ transform
+                                <| "translate(" ++ (toString <| cellWidth / 2 - 10) ++ ",10)" ]
+                            [ if state == Expanded
+                                then downArrow (cellWidth / 2) 10 "aqua"
+                                else upArrow (cellWidth / 2) 10 "aqua"  ]
+                        ]
+                ChoiceItem _ ->
+                    g []
+                        [ g
+                            [ transform
+                                <| "translate(" ++ (toString <| cellWidth / 2 - 10) ++ ",10)" ]
+                            [ case isSelected of
+                                (Just Selected) -> downArrow (cellWidth / 2) 10 "green"
+                                _ -> downArrow (cellWidth / 2) 10 "darkgray" ]
+                        ]
+                Button _ _ ->
+                    g []
+                        [ circle
+                            (circleAttrs (cellWidth / 2) (cellHeight / 2) "darkgray")
+                            []
+                        ]
+        cellLabel cell =
             case cell of
                 Knob label _ -> label
-                Toggle label state -> label ++ " " ++ (if state == TurnedOn then "on" else "off")
+                Toggle label state -> label
                 Button label _ -> label
-                Nested label _ _ -> label ++ " ^"
-                Choice label _ itemChosen _ -> label ++ " ^ (" ++ toString itemChosen ++ ")"
+                Nested label _ _ -> label
+                Choice label _ itemChosen _ -> label
                 ChoiceItem label -> label
-    in svg []
+        labelBackWidth = cellWidth * 2/3
+        labelBackHeight = 20
+    in svg
+        [ width <| toString cellWidth
+        , height <| toString cellHeight
+        ]
         [ cellBody
+        , rect
+            [ x <| toString <| (cellWidth / 2) - (labelBackWidth / 2)
+            , y <| toString <| cellHeight - labelBackHeight
+            , rx "5", ry "5"
+            , width <| toString <| labelBackWidth, height <| toString <| labelBackHeight
+            , fill "rgba(120,0,0,0.4)" ] []
         , text_
-            (textAttrs 10 60)
-            [ Svg.text <| cellLabel ]
+            (textAttrs (cellWidth / 2) (cellHeight - 10) "white")
+            [ Svg.text <| cellLabel cell ]
         ]
